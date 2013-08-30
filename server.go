82991ffdf104d7557394f4ee3f1242803c6e098e
Jacques <jba@zenexity.com>
2013-08-30 14:43:13 +0200

    Allow cross domain

--- server.go
+++ server.go
@@ -30,6 +30,12 @@ func Register(){
   http.ListenAndServe(":9000",nil)
 }
 
+func allowCross(rb *gorest.ResponseBuilder) {
+  rb.AddHeader("Access-Control-Allow-Origin", "*")
+  rb.AddHeader("Access-Control-Allow-Method", "GET, PUT, POST, DELETE")
+  rb.AddHeader("Access-Control-Allow-Headers", "accept, origin, x-requested-with, content-type")
+}
+
 //Position Definition
 type PositionService struct {
   gorest.RestService `root:"/position/" consumes:"application/json" produces:"application/json"`
@@ -37,12 +43,13 @@ type PositionService struct {
   set       gorest.EndPoint `method:"PUT" path:"/" postdata:"Coord"`
 }
 func(serv PositionService) Position() Coord{
-
-    return Coord{123, 12}
+  allowCross(serv.ResponseBuilder())
+  return Coord{123, 12}
 }
 func(serv PositionService) Set(data Coord) {
-    log.Printf("SET")
-    log.Printf("%+v", data)
+  log.Printf("SET")
+  log.Printf("%+v", data)
+  allowCross(serv.ResponseBuilder())
 }
 
 //Rocket Definition
@@ -53,6 +60,7 @@ type RocketService struct {
 func(serv RocketService) Rocket(data Coord) {
     log.Printf("ROCKET")
     log.Printf("%+v", data)
+    allowCross(serv.ResponseBuilder())
 }
 
 // Actions Definition
@@ -64,23 +72,28 @@ type ActionService struct {
     left  gorest.EndPoint `method:"POST" path:"/left" postdata:"string"`
     right gorest.EndPoint `method:"POST" path:"/right" postdata:"string"`
     fire  gorest.EndPoint `method:"PUT"  path:"/fire" postdata:"string"`
-
 }
 func(serv ActionService) Stop(data string) {
   log.Printf("STOP")
+  allowCross(serv.ResponseBuilder())
 }
 func(serv ActionService) Up(data string) {
   log.Printf("UP")
+  allowCross(serv.ResponseBuilder())
 }
 func(serv ActionService) Down(data string) {
   log.Printf("DOWN")
+  allowCross(serv.ResponseBuilder())
 }
 func(serv ActionService) Left(data string) {
   log.Printf("LEFT")
+  allowCross(serv.ResponseBuilder())
 }
 func(serv ActionService) Right(data string) {
   log.Printf("RIGHT")
+  allowCross(serv.ResponseBuilder())
 }
 func(serv ActionService) Fire(data string) {
   log.Printf("FIRE")
+  allowCross(serv.ResponseBuilder())
 }
